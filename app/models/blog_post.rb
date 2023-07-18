6ae5c39e5ad1fbc08c36853b8a3d666fda0deaf4
Peyochanchan <cameleon24@outlook.fr>
2023-07-18 14:49:17 +0200

    onecommit

---
+++ app/models/blog_post.rb
@@ -0,0 +1,33 @@
+# == Schema Information
+#
+# Table name: blog_posts
+#
+#  id           :bigint           not null, primary key
+#  published_at :datetime
+#  title        :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+class BlogPost < ApplicationRecord
+  has_rich_text :content
+  has_one_attached :cover_image
+  validates :title, presence: true
+  validates :content, presence: true
+
+  scope :sorted, -> { order(arel_table[:published_at].desc.nulls_first).order(updated_at: :desc) }
+  scope :draft, -> { where(published_at: nil) }
+  scope :published, -> { where("published_at <= ?", Time.current) }
+  scope :scheduled, -> { where("published_at > ?", Time.current) }
+
+  def draft?
+    published_at.nil?
+  end
+
+  def published?
+    published_at? && published_at <= Time.current
+  end
+
+  def scheduled?
+    published_at? && published_at > Time.current
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "blog_posts#index"
+
+  resources :blog_posts do
+    resource :cover_image, only: :destroy, module: :blog_posts
+  end
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
